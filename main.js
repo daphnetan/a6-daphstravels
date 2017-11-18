335c74ba6959d0317575890a5df1b13618c92a86
daphne tan <htan@microstrategy.com>
2017-11-18 18:47:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,55 +1,116 @@
-// TODO (Step 2): fill in code creating destination objects here
-var destination = {
-    "placesAround": [
+// places objects here
+
+
+//function that passes id from index to destination page
+function show(id){
+    localStorage.setItem("idd",id);
+    window.location.href="destination.html";
+}
+
+var places = {
+    "desiredPlace": [
         {
-            "place": "Paris, France",
+            "name": "Paris, France",
+            //id
             "id": 0,
-            "description": Paris is one of those cities that is so romanticized, you wonder if it's real. And the thing is, it is. This was one of the first cities abroad I traveled to as an adult, and to this day, this trip is one of my favorites. The culture, food, and areas to explore feel endless. It's this perfect mix of tourist attractions and city living. I spent an entire week there, and it didn't feel like quite enough time. I'd definitely go back again to see the amazing art once more in person, and to run through a city so rich in history.
+            //other info
+            "description": "Paris is one of those cities that is so romanticized, you wonder if it's real. And the thing is, it is. This was one of the first cities abroad I traveled to as an adult, and to this day, this trip is one of my favorites. The culture, food, and areas to explore feel endless. It's this perfect mix of tourist attractions and city living. I spent an entire week there, and it didn't feel like quite enough time. I'd definitely go back again to see the amazing art once more in person, and to run through a city so rich in history.",
+            
             //images
-            "img1": "img/paris1.png",
-            "img2": "img/paris2.png",
-            "img3": "img/paris3.png",
-            "img4": "img/paris4.png",
-            "img5": "img/paris5.png",
-            "img6": "img/paris6.png",
+            "img": "img/paris.png",
+            "img1": "img/paris1.jpg",
+            "img2": "img/paris2.jpg",
+            "img3": "img/paris3.jpg",
+            "img4": "img/paris4.jpg",
+            "img5": "img/paris5.jpg",
+            "img6": "img/paris6.jpg",
 
-            //eat recommendations
-            "eatName1": "something goes here", 
-            "eatAddress1": "something goes here",
-            "eatComments1": "something goes here",
+            //eat
+            "eatName1": "Pierre Herme", 
+            "eatAddress1": "72 Rue Bonaparte, 75006 Paris, France",
+            "eatComments1": "We woke up almost every morning and headed to Pierre Herme to get croissants and macarons. Literally, the most indulgent way to start every morning. No regrets.",
 
-            "eatName2": "something goes here",
-            "eatAddress2": "something goes here",
-            "eatComments2": "something goes here",
+            "eatName2": "Septime",
+            "eatAddress2": "80 Rue de Charonne, 75011 Paris, France",
+            "eatComments2": "If you want a trendy place to eat but want fair prices, head to Septime for lunch. I remember eating a vegetable dish with a poached egg—sounds ordinary, but it was out of this world. Call in ahead of time to make a reservation. Seriously, do it.",
 
-            "eatName3": "something goes here",
-            "eatAddress3": "something goes here",
-            "eatComments3": "something goes here",
+            "eatName3": "Berthillon",
+            "eatAddress3": "29-31 Rue Saint-Louis en l'Île, 75004 Paris, France",
+            "eatComments3": "I ate two ice cream cones one morning because it was that good. In fact, I ate one before the Notre Dame and another later that evening. The flavoring is so deep and rich, especially the Pistachio and Hazelnut. No regrets.",
 
-            //play recommendations
-            "playName1": "something goes here", 
-            "playAddress1": "something goes here",
-            "playComments1": "something goes here",
+            // play
+            "playName1": "Notre-Dame de Paris", 
+            "playAddress1": "6 Parvis Notre-Dame - Pl. Jean-Paul II, 75004 Paris, France",
+            "playComments1": "One of my favorite days in Paris was when  I climbed the Notre-Dame (387 steps!) first thing in the morning. It was a crisp, fall morning and I got a full view of Parisian bustle. The Eiffel Tower looked ever so picturesque from here, making the climb worth the wait.",
 
-            "playName2": "something goes here",
-            "playAddress2": "something goes here",
-            "playComments2": "something goes here",
+            "playName2": "Musée d'Orsay",
+            "playAddress2": "1 Rue de la Légion d'Honneur, 75007 Paris, France",
+            "playComments2": "The architecture of this museum alone is worth the visit. A former railway station, this museum hosts paintings from famed artists I had only seen in history books and museums at home. I could spend an entire afternoon just lounging around.",
 
-            "playName3": "something goes here",
-            "playAddress3": "something goes here",
-            "playComments3": "something goes here",
+            "playName3": "Monet’s Residence",
+            "playAddress3": "84 Rue Claude Monet, 27620 Giverny, France",
+            "playComments3": "Growing up, I would attempt to engage with my inner Monet. Nothing beat going a little outside of Paris for half a day to see the inspiration for Monet’s creations was breathtaking. I also got to walk up and down his street, to get a better sense the cozy corners of the real Parisian lifestyle.",
 
-            //stay recommendation
-            "stayName": "something goes here", 
-            "stayAddress": "something goes here",
-            "stayComments": "something goes here"
+            //stay
+            "stayName": "Notre-Dame de Paris", 
+            "stayAddress": "6 Parvis Notre-Dame - Pl. Jean-Paul II, 75004 Paris, France",
+            "stayComments": "I'm not sure if our Airbnb is still in operation. It wasn’t necessarily a place I would recommend. The area, however, was great and conveniently located. We could get almost anywhere by foot, especially to Pierre Herme and the museums. Priorities, people.",
+            
+        },
+        {
+            "name": "Sevilla, Spain",
+            "id": 1,
+            "description": "Seville is the capital of southern Spain’s Andalusia region. It's famous for flamenco dancing, particularly in its Triana neighborhood. Major landmarks include the ornate Alcázar castle complex, built during the Moorish Almohad dynasty, and the 18th-century Plaza de Toros de la Maestranza bullring. The Gothic Seville Cathedral is the site of Christopher Columbus’s tomb and a minaret turned bell tower, the Giralda.",
+            
+            //images
+            "img": "img/sevilla.jpg",
+            "img1": "img/sevilla1.jpg",
+            "img2": "img/sevilla2.jpg",
+            "img3": "img/sevilla3.jpg",
+            "img4": "img/sevilla4.jpg",
+            "img5": "img/sevilla5.jpg",
+            "img6": "img/sevilla6.jpg",
+
+             //eat
+             "eatName1": "Pierre Herme", 
+             "eatAddress1": "72 Rue Bonaparte, 75006 Paris, France",
+             "eatComments1": "We woke up almost every morning and headed to Pierre Herme to get croissants and macarons. Literally, the most indulgent way to start every morning. No regrets.",
+ 
+             "eatName2": "Septime",
+             "eatAddress2": "80 Rue de Charonne, 75011 Paris, France",
+             "eatComments2": "If you want a trendy place to eat but want fair prices, head to Septime for lunch. I remember eating a vegetable dish with a poached egg—sounds ordinary, but it was out of this world. Call in ahead of time to make a reservation. Seriously, do it.",
+ 
+             "eatName3": "Berthillon",
+             "eatAddress3": "29-31 Rue Saint-Louis en l'Île, 75004 Paris, France",
+             "eatComments3": "I ate two ice cream cones one morning because it was that good. In fact, I ate one before the Notre Dame and another later that evening. The flavoring is so deep and rich, especially the Pistachio and Hazelnut. No regrets.",
+ 
+             // play
+             "playName1": "let's play", 
+             "playAddress1": "testy test test",
+             "playComments1": "One of my favorite days in Paris was when  I climbed the Notre-Dame (387 steps!) first thing in the morning. It was a crisp, fall morning and I got a full view of Parisian bustle. The Eiffel Tower looked ever so picturesque from here, making the climb worth the wait.",
+ 
+             "playName2": "Musée d'Orsay",
+             "playAddress2": "1 Rue de la Légion d'Honneur, 75007 Paris, France",
+             "playComments2": "The architecture of this museum alone is worth the visit. A former railway station, this museum hosts paintings from famed artists I had only seen in history books and museums at home. I could spend an entire afternoon just lounging around.",
+ 
+             "playName3": "Monet’s Residence",
+             "playAddress3": "84 Rue Claude Monet, 27620 Giverny, France",
+             "playComments3": "Growing up, I would attempt to engage with my inner Monet. Nothing beat going a little outside of Paris for half a day to see the inspiration for Monet’s creations was breathtaking. I also got to walk up and down his street, to get a better sense the cozy corners of the real Parisian lifestyle.",
+ 
+             //stay
+             "stayName": "daphne's haus", 
+             "stayAddress": "6 Parvis Notre-Dame - Pl. Jean-Paul II, 75004 Paris, France",
+             "stayComments": "I'm not sure if our Airbnb is still in operation. It wasn’t necessarily a place I would recommend. The area, however, was great and conveniently located. We could get almost anywhere by foot, especially to Pierre Herme and the museums. Priorities, people.", 
+            
         },
 
         {
-            "place": "Lisbon, Portugal",
-            "id": 1,
+            "name": "Lisbon, Portugal",
+            "id": 2,
+            "description": "The United Kingdom, made up of England, Scotland, Wales and Northern Ireland, is an island nation in northwestern Europe. England – birthplace of Shakespeare and The Beatles – is home to the capital, London, a globally influential centre of finance and culture. England is also site of Neolithic Stonehenge, Bath’s Roman spa and centuries-old universities at Oxford and Cambridge.",
 
             //images
+            "img": "img/lisbon.jpeg",
             "img1": "img/lisbon1.jpeg",
             "img2": "img/lisbon2.jpeg",
             "img3": "img/lisbon3.jpeg",
@@ -57,76 +118,130 @@ var destination = {
             "img5": "img/lisbon5.jpeg",
             "img6": "img/lisbon6.jpeg",
 
-            //eat recommendations
-            "eatName1": "something goes here", 
-            "eatAddress1": "something goes here",
-            "eatComments1": "something goes here",
-
-            "eatName2": "something goes here",
-            "eatAddress2": "something goes here",
-            "eatComments2": "something goes here",
-
-            "eatName3": "something goes here",
-            "eatAddress3": "something goes here",
-            "eatComments3": "something goes here",
-
-            //play recommendations
-            "playName1": "something goes here", 
-            "playAddress1": "something goes here",
-            "playComments1": "something goes here",
-
-            "playName2": "something goes here",
-            "playAddress2": "something goes here",
-            "playComments2": "something goes here",
-
-            "playName3": "something goes here",
-            "playAddress3": "something goes here",
-            "playComments3": "something goes here",
-
-            //stay recommendation
-            "stayName": "something goes here", 
-            "stayAddress": "something goes here",
-            "stayComments": "something goes here"
+             //eat
+             "eatName1": "Pierre Herme", 
+             "eatAddress1": "72 Rue Bonaparte, 75006 Paris, France",
+             "eatComments1": "We woke up almost every morning and headed to Pierre Herme to get croissants and macarons. Literally, the most indulgent way to start every morning. No regrets.",
+ 
+             "eatName2": "Septime",
+             "eatAddress2": "80 Rue de Charonne, 75011 Paris, France",
+             "eatComments2": "If you want a trendy place to eat but want fair prices, head to Septime for lunch. I remember eating a vegetable dish with a poached egg—sounds ordinary, but it was out of this world. Call in ahead of time to make a reservation. Seriously, do it.",
+ 
+             "eatName3": "Berthillon",
+             "eatAddress3": "29-31 Rue Saint-Louis en l'Île, 75004 Paris, France",
+             "eatComments3": "I ate two ice cream cones one morning because it was that good. In fact, I ate one before the Notre Dame and another later that evening. The flavoring is so deep and rich, especially the Pistachio and Hazelnut. No regrets.",
+ 
+             // play
+             "playName1": "Notre-Dame de Paris", 
+             "playAddress1": "6 Parvis Notre-Dame - Pl. Jean-Paul II, 75004 Paris, France",
+             "playComments1": "One of my favorite days in Paris was when  I climbed the Notre-Dame (387 steps!) first thing in the morning. It was a crisp, fall morning and I got a full view of Parisian bustle. The Eiffel Tower looked ever so picturesque from here, making the climb worth the wait.",
+ 
+             "playName2": "Musée d'Orsay",
+             "playAddress2": "1 Rue de la Légion d'Honneur, 75007 Paris, France",
+             "playComments2": "The architecture of this museum alone is worth the visit. A former railway station, this museum hosts paintings from famed artists I had only seen in history books and museums at home. I could spend an entire afternoon just lounging around.",
+ 
+             "playName3": "Monet’s Residence",
+             "playAddress3": "84 Rue Claude Monet, 27620 Giverny, France",
+             "playComments3": "Growing up, I would attempt to engage with my inner Monet. Nothing beat going a little outside of Paris for half a day to see the inspiration for Monet’s creations was breathtaking. I also got to walk up and down his street, to get a better sense the cozy corners of the real Parisian lifestyle.",
+ 
+             //stay
+             "stayName": "toodles", 
+             "stayAddress": "6 Parvis Notre-Dame - Pl. Jean-Paul II, 75004 Paris, France",
+             "stayComments": "I'm not sure if our Airbnb is still in operation. It wasn’t necessarily a place I would recommend. The area, however, was great and conveniently located. We could get almost anywhere by foot, especially to Pierre Herme and the museums. Priorities, people.",          
         },
+        
         {
-            "place": "Copenhagen, Denmark",
-            "img": "img/copenhagen.jpg",
-            "id": 2
-        }
+            "name": "Copenhagen, Denmark",
+            //id
+            "id": 3,
+        },
+
+        {
+            "name": "UK + Ireland",
+            //id
+            "id": 4,
+        },
+
+        {
+            "name": "Marrakech, Morocco",
+            //id
+            "id": 5,
+        },
     ]
 }
 
 $(document).ready(function(){
-    // TODO (Step 4): fill in code for changing selection-template here
-    
     // get the HTML template using jQuery
     var source = $("#selection-template").html();
     // compile the template into a function
     var template = Handlebars.compile(source);
     // create new HTML using our data
-    var newHTML = template(destination);
+    var newHTML = template(places);
     // add the new HTML to the page
     $("#product-selection-container").append(newHTML);
 
-    // TODO (Step 7): add function call for 1st update to detail-template
-    updateProductDetail(0);
+    // //function call for 1st update to detail-template
+    i=Number(localStorage.getItem("idd"));
+    updateProductDetail(i);
+    updateEatDetail(i);
+    updatePlayDetail(i);
+    updateStayDetail(i);
 
-    // TODO: (Step 8): add code below for changing product details on click
-    $(".product-selection").click(function(){
-        var id = $(this).attr("id");
-        updateProductDetail(id);
-    });
-
+    //change product details on click
+    // $(".product-selection").click(function() {
+    //     var idd = $(this).attr("id");
+    //     updateProductDetail(idd);
+    //     updateEatDetail(idd);
+    //     updatePlayDetail(idd);
+    //     updateStayDetail(idd);
+    // });
 });
 
-// TODO (Step 6): fill in function for updating detail-template based on id
+//update detail-template based on id
 function updateProductDetail(id) {
-  // get the HTML template using jQuery
-  var source = $("#detail-template").html();
-  // compile the template into a function
-  var template = Handlebars.compile(source);
-  // create new HTML using our data
-  var newHTML = template(destination.placesAround[id]);
-  // add the new HTML to the page
-  $("#destination-container").html(newHTML);
+    // get the HTML template using jQuery
+    var source = $("#detail-template").html();
+    // compile the template into a function
+    var template = Handlebars.compile(source);
+    // create new HTML using our data
+    var newHTML = template(places.desiredPlace[id]);
+    // add the new HTML to the page
+    $("#product-detail-container").html(newHTML);
 }
+
+//updating play-template based on id
+function updateEatDetail(id) {
+    // get the HTML template using jQuery
+    var source = $("#eat-template").html();
+    // compile the template into a function
+    var template = Handlebars.compile(source);
+    // create new HTML using our data
+    var newHTML = template(places.desiredPlace[id]);
+    // add the new HTML to the page
+    $("#eat-detail-container").html(newHTML);
+}
+
+//updating play-template based on id
+function updatePlayDetail(id) {
+    // get the HTML template using jQuery
+    var source = $("#play-template").html();
+    // compile the template into a function
+    var template = Handlebars.compile(source);
+    // create new HTML using our data
+    var newHTML = template(places.desiredPlace[id]);
+    // add the new HTML to the page
+    $("#play-detail-container").html(newHTML);
+}
+
+//updating stay-template based on id
+function updateStayDetail(id) {
+    // get the HTML template using jQuery
+    var source = $("#stay-template").html();
+    // compile the template into a function
+    var template = Handlebars.compile(source);
+    // create new HTML using our data
+    var newHTML = template(places.desiredPlace[id]);
+    // add the new HTML to the page
+    $("#stay-detail-container").html(newHTML);
+}
+
